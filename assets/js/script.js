85df89652f432013b45b8c61006820737436335e
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-11-10 14:37:00 +0100

    fixed layout and img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-console.log("Hello world")
-
 function sendMail(contactForm) {
     emailjs.send("gmail", "Christian", {
         "from_name": contactForm.fullname.value,
@@ -14,4 +12,22 @@ function sendMail(contactForm) {
         console.log("FAILD!", error)
     });
     return false;
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
